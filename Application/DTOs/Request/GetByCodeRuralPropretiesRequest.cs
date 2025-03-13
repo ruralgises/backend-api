b3024d61f10a11717e91ae00a22a908b14f2d02c
Heflain da Rocha Mendes <heflain@outlook.com.br>
2025-03-13 00:56:24 -0300

    mudando o termo de IsOfficial para IsCovermental

--- Application/DTOs/Request/GetByCodeRuralPropretiesRequest.cs
+++ Application/DTOs/Request/GetByCodeRuralPropretiesRequest.cs
@@ -5,7 +5,7 @@ namespace Application.DTOs.Request
     public class GetByCodeRuralPropretiesRequest
     {
         [Required]
-        [RegularExpression(@"^BA-\d{7}-[0-9A-F]{32}$", ErrorMessage = "O formato da string é inválido.")]
+        [RegularExpression(@"^[A-Z]{2}-\d{7}-[0-9A-F]{32}$", ErrorMessage = "O formato da string é inválido.")]
         [StringLength(43, MinimumLength = 43, ErrorMessage = "A string deve ter exatamente 43 caracteres.")]
         public string Code { get; set; }
     }
---
+++ Persistence/Migrations/20250223175701_UpdateIsOfficialToIsGovermental.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateIsOfficialToIsGovermental : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsOfficial",
+                table: "informationDatabases",
+                newName: "IsGovernmental");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsGovernmental",
+                table: "informationDatabases",
+                newName: "IsOfficial");
+        }
+    }
+}
